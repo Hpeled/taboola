10853e034b7e7ef9f8228af6073cba06bbc82f99
Hpeled <74018137+Hpeled@users.noreply.github.com>
2025-11-09 17:40:24 +0200

    .

--- widget.js
+++ widget.js
@@ -1,7 +1,7 @@
 const WIDGET_CONTAINER_ID = "recommendations-widget";
 
 const BASE_API_URL =
-  "http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&count=8&source.type=video&source.id=214321562187&source.url=http://www.site.com/videos/214321562187.htmlד";
+  "http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&count=8&source.type=video&source.id=214321562187&source.url=http://www.site.com/videos/214321562187.html";
 
 const USE_MOCK_DATA = true;
 
